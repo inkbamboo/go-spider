9fbd9a858e164a6a2a0d0886de03424da8402dc0
liuxinlei <liuxinlei@popmart.com>
2024-07-04 16:55:10 +0800

    修改

--- packages/kespider/internal/scripts/server.go
+++ packages/kespider/internal/scripts/server.go
@@ -2,10 +2,8 @@ package scripts
 
 import (
 	"fmt"
-	"github.com/inkbamboo/ares"
-	"github.com/inkbamboo/go-spider/packages/kespider/internal/model"
+	"github.com/inkbamboo/go-spider/packages/kespider/internal/services"
 	"github.com/inkbamboo/go-spider/packages/kespider/internal/spiders"
-	"github.com/samber/lo"
 	"os"
 	"time"
 )
@@ -31,41 +29,12 @@ func RunChengJiaoSpider(city string) {
 	//}
 	select {}
 }
+
 func RunTest() {
 	fmt.Println("RunTest")
-	tx := ares.Default().GetOrm("sjz")
-	version1 := "2024-06-26"
-	version2 := "2024-06-30"
-	var hosePrices []*model.HousePrice
-	_ = tx.Model(&model.HousePrice{}).Where("version in(?)", []string{version1, version2}).Find(&hosePrices).Error
-	houseInfos := lo.GroupBy(hosePrices, func(item *model.HousePrice) string {
-		return item.HousedelId + item.DistrictId
-	})
-	var sellOutHouse, newHouse, changeHouse string
-	for _, houseInfo := range houseInfos {
-		var oldPrice, newPrice float64
-		for _, item := range houseInfo {
-			if item.Version == version1 {
-				oldPrice = item.TotalPrice
-			} else if item.Version == version2 {
-				newPrice = item.TotalPrice
-			}
-		}
-		if oldPrice > 0 && newPrice > 0 && oldPrice != newPrice {
-			changeHouse += fmt.Sprintf("%s,%v,%v\n", houseInfo[0].HousedelId, oldPrice, newPrice)
-			continue
-		}
-		if newPrice == 0 {
-			sellOutHouse += fmt.Sprintf("%s\n", houseInfo[0].HousedelId)
-			continue
-		}
-		if oldPrice == 0 {
-			newHouse += fmt.Sprintf("%s\n", houseInfo[0].HousedelId)
-			continue
-		}
-	}
-	os.WriteFile("./temp/sell_out_house.csv", []byte(sellOutHouse), 0644)
-	os.WriteFile("./temp/new_house.csv", []byte(newHouse), 0644)
+	versions := []string{"2024-06-26", "2024-06-30", "2024-07-03"}
+	changeHouse := services.GetHousePriceService().GetChangeHouse(versions)
 	os.WriteFile("./temp/change_house.csv", []byte(changeHouse), 0644)
-	select {}
+	time.Sleep(10 * time.Second)
+	fmt.Println("RunTest End")
 }
--- packages/kespider/internal/services/housePrice.service.go
+++ packages/kespider/internal/services/housePrice.service.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/inkbamboo/ares"
 	"github.com/inkbamboo/go-spider/packages/kespider/internal/model"
+	"github.com/samber/lo"
 	"gorm.io/gorm/clause"
+	"strings"
 	"sync"
 )
 
@@ -37,4 +39,40 @@ func (s *HousePriceService) SaveHousePrice(housePrice *model.HousePrice, city st
 		return
 	}
 	return
+}
+func (s *HousePriceService) GetChangeHouse(versions []string) string {
+	tx := ares.Default().GetOrm("sjz")
+	var hosePrices []*model.HousePrice
+	_ = tx.Model(&model.HousePrice{}).Where("version in(?)", versions).Find(&hosePrices).Error
+	houseInfos := lo.GroupBy(hosePrices, func(item *model.HousePrice) string {
+		return item.HousedelId + item.DistrictId
+	})
+	changeHouse := fmt.Sprintf("'housedel_id,%s", strings.Join(versions, ","))
+	changeHouse += fmt.Sprintf("'\n")
+	for _, houseInfo := range houseInfos {
+		housePrice := map[string]float64{}
+		for _, item := range houseInfo {
+			housePrice[item.Version] = item.TotalPrice
+		}
+		line := fmt.Sprintf("'%s", houseInfo[0].HousedelId)
+		var curlPrice float64
+		var isChange bool
+		for _, version := range versions {
+			line += fmt.Sprintf(",%v", housePrice[version])
+			if housePrice[version] == 0 {
+				continue
+			}
+			if curlPrice == 0 {
+				curlPrice = housePrice[version]
+			}
+			if !isChange && curlPrice > housePrice[version] {
+				isChange = true
+			}
+		}
+		line += fmt.Sprintf("'\n")
+		if isChange {
+			changeHouse += line
+		}
+	}
+	return changeHouse
 }
