8f2d0bc3666b7dccca0b9e2264df4febd56a7396
liuxinlei <liuxinlei@popmart.com>
2025-05-14 09:52:01 +0800

    修改

--- packages/poetryspider/internal/spiders/zhsc/poetry.spider.go
+++ packages/poetryspider/internal/spiders/zhsc/poetry.spider.go
@@ -59,7 +59,7 @@ func (s *PoetrySpider) startPoetry(poetryType string) {
 		totalPage := cast.ToInt64(strings.TrimSpace(e.DOM.Find("li").Find("a").Last().Text()))
 		if curPage < totalPage {
 			c.UserAgent = browser.Random()
-			time.Sleep(200 * time.Millisecond)
+			time.Sleep(500 * time.Millisecond)
 			c.Visit(fmt.Sprintf("https://zhsc.org/%s/page-%d.htm", poetryType, curPage+1))
 		}
 	})
@@ -85,7 +85,7 @@ func (s *PoetrySpider) startPoetry(poetryType string) {
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
-	c.Visit(fmt.Sprintf("https://zhsc.org/%s/page-120.htm", poetryType))
+	c.Visit(fmt.Sprintf("https://zhsc.org/%s/page-1840.htm", poetryType))
 }
 func (s *PoetrySpider) parsePoetry(poetryId, poetryType string, e *goquery.Selection) {
 	poetry := &model.Poetry{}
